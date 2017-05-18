e63ded469a8bb7b5e11844aac30a12d8bbf88636
Nandor Galambosi <nandor.galambosi@gmail.com>
2017-05-18 16:50:31 +0100

    Additional logging added to streams.sh

--- tihany/scripts/streams.sh
+++ tihany/scripts/streams.sh
@@ -3,13 +3,14 @@
 LIVEPIDFILE=/tmp/live555.pid
 LIVEOUT=/tmp/live.out
 PYPIDFILE=/tmp/streams.pid
+PYOUT=/tmp/py.out
 self_dir=`dirname "$(readlink -f $0)"`
 source ${self_dir}/utils.sh
 source ~pi/.params
 #source test_params
 SELF_IP=`ip -o -4 address show $DEVICE |sed 's#.*inet \([^ /]*\) \?/\?.*#\1#'`
 export DB SELF_IP RTSP_PORT LIVEPIDFILE LIVEOUT
-
+msg "$HOST Stream proxy service restarted. Ethernet IP: $SELF_IP"
 stopall() {
     kill `cat $PYPIDFILE`
     kill `cat $LIVEPIDFILE`
@@ -23,15 +24,17 @@ if [ ! -f $DB ];then
   sql "create table streams (id INTEGER,url TEXT)"
 fi
 
-python3 $self_dir/streams.py &
+python3 $self_dir/streams.py &>$PYOUT &
 echo $! >$PYPIDFILE
 
 while [ 0 ];do
-    killall $LIVEBIN
+    killall `basename $LIVEBIN`
     if [ `sql "SELECT url from streams ORDER BY id"|wc -l` -gt 0 ];then
-        sql "SELECT url from streams ORDER BY id"|xargs $LIVEBIN &>$LIVEOUT &
+        echo -n "$LIVEBIN" >$LIVEOUT
+        sql "SELECT url from streams ORDER BY id"|xargs  >>$LIVEOUT
+        sql "SELECT url from streams ORDER BY id"|xargs $LIVEBIN &>>$LIVEOUT &
         sleep 5
-        pidof $LIVEBIN >$LIVEPIDFILE
+        pidof `basename $LIVEBIN` >$LIVEPIDFILE
     else
         echo "No streams defined." >$LIVEOUT
         sleep 3600 &
